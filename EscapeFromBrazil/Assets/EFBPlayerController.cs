03b1e709223509ba2e1b1dcb3f850474cb286f09
Osvak <oscarcanales2001@gmail.com>
2022-10-23 13:06:10 +0200

    set up

---
+++ EscapeFromBrazil/Assets/EFBPlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EFBPlayerController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject bullet; 
+    [SerializeField] private Transform bulletDir;
+    private EFBActions controls;
+
+    private void Awake()
+    {
+        controls = new EFBActions();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+    void Start()
+    {
+        controls.Player.Shoot.performed += ctx => PlayerShoot();
+    }
+
+    private void PlayerShoot()
+    {
+        Vector2 mousePosition = controls.Player.MousePosition.ReadValue<Vector2>();
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        GameObject g = Instantiate(bullet, bulletDir.position, bulletDir.rotation);
+        g.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
