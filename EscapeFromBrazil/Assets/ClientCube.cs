8afb593781032e84ec0cea87e7774267d7b98f7c
Mario HerFer <41444696+MHF13@users.noreply.github.com>
2022-11-03 09:01:05 +0100

    Solved Errors

--- EscapeFromBrazil/Assets/ClientCube.cs
+++ EscapeFromBrazil/Assets/ClientCube.cs
@@ -1,10 +1,25 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
+using System.Threading;
 
 public class ClientCube : MonoBehaviour
 {
-    public string Ip;
+    Socket newSocket;
+    IPEndPoint ipep;
+    EndPoint Server;
+
+    Thread CurrentThread;
+
+    public string IP, message;
+    private string newMessage;
+
+    bool updateText = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +29,40 @@ public class ClientCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.C)){
+            startConexion();
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            newMessage = "\n[" + userName + "]:" + messageField.text;
-
-        messageField.text = "";
-
-        Debug.Log("Message has been sent");
+            Debug.Log("send:" + message);
         
-        byte[] data = Encoding.ASCII.GetBytes(newMessage);
+            byte[] data = Encoding.ASCII.GetBytes(message);
 
-        newSocket.SendTo(data, data.Length, SocketFlags.None, Server);
+            newSocket.SendTo(data, data.Length, SocketFlags.None, Server);
         }
+
+        if (updateText)
+        {
+
+            Debug.Log("New Message: " + newMessage);
+
+            updateText = false;
+
+        }
+    }
+
+    private void startConexion(){
+        Debug.Log("Conectado al servidor");
+
+        newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        ipep = new IPEndPoint(IPAddress.Any, 6799); // IPAddress.Any, puerto del cliente
+        newSocket.Bind(ipep);
+        Server = new IPEndPoint(IPAddress.Parse(IP), 8000);
+        newSocket.Connect(Server);
+        
+        CurrentThread = new Thread(Conexion);
+        CurrentThread.Start();
     }
 
     private void Conexion()
--- EscapeFromBrazil/Assets/ServerCube.cs
+++ EscapeFromBrazil/Assets/ServerCube.cs
@@ -1,18 +1,86 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
+using System.Threading;
 
 public class ServerCube : MonoBehaviour
 {
+    Socket newSocket;
+    IPEndPoint ipep;
+    EndPoint Client;
+
+    bool updateText;
+
+    string newMessage,message;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        ipep = new IPEndPoint(IPAddress.Any, 8000); // un puerto para el host, IPAddress.Any
+        newSocket.Bind(ipep);
+
+        // inicializar Client
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        Client = (EndPoint)(sender);
+
+        Thread thread = new Thread(Reciver);
+
+        thread.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (updateText)
+        {
+            Debug.Log("Modified text");
+
+            Debug.Log(message);
+
+            byte[] data = new byte[255];
+
+            data = Encoding.ASCII.GetBytes(message);
+
+            newSocket.SendTo(data, data.Length, SocketFlags.None, Client);
+
+            updateText = false;
+
+        }
+    }
+
+    private void Reciver()
+    {
+        while (true)
+        {
+            byte[] data = new byte[255];
+            int recv = newSocket.ReceiveFrom(data, ref Client);
+
+            string str = Encoding.ASCII.GetString(data);
+
+            newMessage = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != 0) { newMessage += str[i]; }
+                else break;
+
+            }
+
+            Debug.Log("Receives a user");
+
+            byte[] invitation;
+            invitation = Encoding.ASCII.GetBytes("Can Join");
+            newSocket.SendTo(invitation, invitation.Length, SocketFlags.None, Client);
+
+            message = ">> " + newMessage;
+
+            updateText = true;
+
+        }
     }
 }
