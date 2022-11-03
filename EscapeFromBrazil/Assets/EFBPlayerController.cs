6067f5d71dc4bbb13be4e91ca685dde6cc021590
Osvak <61042986+Osvak@users.noreply.github.com>
2022-11-03 09:51:59 +0100

    movement and aim

--- EscapeFromBrazil/Assets/EFBPlayerController.cs
+++ EscapeFromBrazil/Assets/EFBPlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EFBPlayerController : MonoBehaviour
 {
@@ -9,11 +10,17 @@ public class EFBPlayerController : MonoBehaviour
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform bulletDir;
     [SerializeField] private Transform trash;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float movementVel = 2f;
     private EFBActions controls;
+    private bool canShoot = true;
+    [SerializeField] LayerMask ignore;
+
 
     private void Awake()
     {
         controls = new EFBActions();
+        ignore = LayerMask.NameToLayer("Player");
     }
 
     private void OnEnable()
@@ -31,17 +38,45 @@ public class EFBPlayerController : MonoBehaviour
         controls.Player.Movement.performed += ctx => PlayerMove();
     }
 
+    void Update()
+    {
+        PlayerAim();
+        PlayerMove();
+    }
+
+    private void PlayerAim()
+    {
+        Vector3 mouseScreenPos = controls.Player.MousePosition.ReadValue<Vector2>();
+        mouseScreenPos.z = 100f;
+        mouseScreenPos = cam.ScreenToWorldPoint(mouseScreenPos);
+        Ray ray = cam.ScreenPointToRay(new Vector3(controls.Player.MousePosition.ReadValue<Vector2>().x, controls.Player.MousePosition.ReadValue<Vector2>().y, 100f));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f, ~ignore))
+        {
+            transform.LookAt(new Vector3(hit.point.x, 1.31f, hit.point.z));
+        }
+
+        
+    }
+
     private void PlayerMove()
     {
-        Vector2 movement = controls.Player.Movement.ReadValue<Vector2>();
-
+        Vector3 movement = new Vector3(controls.Player.Movement.ReadValue<Vector2>().x, 0f, controls.Player.Movement.ReadValue<Vector2>().y) * movementVel;
+        transform.position += movement * Time.deltaTime;
     }
 
     private void PlayerShoot()
     {
-        Vector2 mousePosition = controls.Player.MousePosition.ReadValue<Vector2>();
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        if (!canShoot) return;;
         GameObject g = Instantiate(bullet, bulletDir.position, bulletDir.rotation, trash);
         g.SetActive(true);
+        StartCoroutine(CanShoot());
+    }
+
+    IEnumerator CanShoot()
+    {
+        canShoot = false;
+            yield return new WaitForSeconds(.5f);
+        canShoot = true;
     }
 }
