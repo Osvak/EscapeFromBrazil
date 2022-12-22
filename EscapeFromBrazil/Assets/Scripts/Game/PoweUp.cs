d2f2d879c9afc95bcdd844e05315b69871639c10
Mario HerFer <41444696+MHF13@users.noreply.github.com>
2022-12-22 11:09:45 +0100

    PowerUp sprite Sistem

--- EscapeFromBrazil/Assets/Scripts/Game/PoweUp.cs
+++ EscapeFromBrazil/Assets/Scripts/Game/PoweUp.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PoweUp : MonoBehaviour
 {
@@ -10,9 +11,12 @@ public class PoweUp : MonoBehaviour
     public int type = 0;
     private bool delete = false;
     private int ID;
+    [Header("\nPowerUp list:\nLive + 1\nFire rate")]
+    public List<Sprite> sprites = new List<Sprite> ();
     public void StartSpawn(Vector3 pos, int _type,int id)
     {
         powerUpManager = this.GetComponentInParent<PowerUpManager>();
+        this.GetComponent<SpriteRenderer>().sprite = sprites[_type - 1];
         transform.position = pos;
         type = _type;
         ID = id;
